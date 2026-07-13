2736629246c25593ecdeb255f7cb7453f23d1a5c
akula_jashwanth_79 <jashwanthakula789@gmail.com>
2026-07-14 03:32:52 +0530

    Time: 1 ms (99.85%), Space: 46.3 MB (96.15%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,75 +1,41 @@
-class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    List<List<String>> ans = new ArrayList<>();
+class Solution { 
+    public List<List<String>> solveNQueens(int n) { 
+        List<List<String>> r = new ArrayList<>(); 
+        char[][] board = new char[n][n]; 
+        for (int i = 0; i < n; i++) { 
+            Arrays.fill(board[i], '.'); 
+        } 
+        dfs(n, 0, new boolean[n], new boolean[2 * n - 1], new boolean[2 * n - 1], board, r); 
+        return r; 
+    } 
 
-    public List<List<String>> solveNQueens(int n) {
+    public void dfs(int n, int i, boolean[] col, boolean[] d1, boolean[] d2, char[][] board, List<List<String>> r) { 
+        if (i == n) { 
+            r.add(construct(board)); 
+            return; 
+        } 
+        for (int j = 0; j < n; j++) { 
+            if (col[j] || d1[i + j] || d2[n - 1 + i - j]) continue; 
+            
+            board[i][j] = 'Q'; 
+            col[j] = d1[i + j] = d2[n - 1 + i - j] = true; 
+            
+            dfs(n, i + 1, col, d1, d2, board, r); 
+            
+            board[i][j] = '.'; 
+            col[j] = d1[i + j] = d2[n - 1 + i - j] = false; 
+        } 
+    } 
 
-        char[][] board = new char[n][n];
-
-        // Fill the board with '.'
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
-        }
-
-        // Start placing queens from row 0
-        solve(board, 0);
-
-        return ans;
-    }
-
-    private void solve(char[][] board, int row) {
-
-        // Base case: All queens are placed
-        if (row == board.length) {
-
-            List<String> temp = new ArrayList<>();
-
-            // Convert board into List<String>
-            for (char[] r : board) {
-                temp.add(new String(r));
-            }
-
-            ans.add(temp);
-            return;
-        }
-
-        // Try every column in the current row
-        for (int col = 0; col < board.length; col++) {
-
-            if (isSafe(board, row, col)) {
-
-                // Place queen
-                board[row][col] = 'Q';
-
-                // Recur for next row
-                solve(board, row + 1);
-
-                // Backtrack
-                board[row][col] = '.';
-            }
-        }
-    }
-
-    private boolean isSafe(char[][] board, int row, int col) {
-
-        // Check same column
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q')
-                return false;
-        }
-
-        // Check upper-left diagonal
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q')
-                return false;
-        }
-
-        // Check upper-right diagonal
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q')
-                return false;
-        }
-
-        return true;
-    }
+    public List<String> construct(char[][] board) { 
+        List<String> l = new ArrayList<>(); 
+        for (int i = 0; i < board.length; i++) { 
+            l.add(String.valueOf(board[i])); 
+        } 
+        return l; 
+    } 
 }
