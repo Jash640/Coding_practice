8c13c5c2b1766cfb7e1786d471231433880cb833
akula_jashwanth_79 <jashwanthakula789@gmail.com>
2026-07-14 16:26:38 +0530

    Time: 1 ms (87.04%), Space: 43.8 MB (97.49%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>>list=new ArrayList<>();
+        dfs(list,new ArrayList<>(),nums,0);
+        return list;
+    }
+    public void dfs(List<List<Integer>>list,List<Integer>slist,int[] nums,int start){
+        list.add(new ArrayList<>(slist));
+        for(int i=start;i<nums.length;i++){
+            slist.add(nums[i]);
+            dfs(list,slist,nums,i+1);
+            slist.remove(slist.size()-1);
+        }
+    }
+}
